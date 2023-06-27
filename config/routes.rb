b10e384180370587342852757601198a5ff42135
Clatirissa Batista <clarissapbatista@gmail.com>
2023-06-27 17:51:28 +0100

    services cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/terms&privacy', to: 'pages#terms&privacy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
